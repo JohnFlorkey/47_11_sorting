a96028d37086a17a7e42a7c2054cf5dfd718f475
John Florkey <john.florkey@gmail.com>
2021-10-13 23:16:36 -0400

    Completed exercise

---
+++ merge.js
@@ -0,0 +1,35 @@
+function merge(arr1, arr2) {
+  const result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return result;
+}
+
+function mergeSort(array) {
+  if (array.length <= 1) return array;
+  const mid = Math.floor(array.length / 2);
+  let leftArray = mergeSort(array.slice(0, mid));
+  let rightArray = mergeSort(array.slice(mid));
+
+  return merge(leftArray, rightArray);
+}
+
+module.exports = { merge, mergeSort };
